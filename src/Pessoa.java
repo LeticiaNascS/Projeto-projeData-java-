d9e065b158076f0fc374f2691c0563262994cc0f
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-04-17 18:54:52 -0300

    Criando a classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Pessoa{
+    private string nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa (string nome, LocalDate dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+}
